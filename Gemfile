238f5b4c31dda55e6b5af575a1e15a235b22b227
Nicki Nixon <nickitza@gmail.com>
2019-05-09 11:33:09 -0600

    add colorize gem to main

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+#gemfiles need a source, ^ most popular
+#can also run bundle init to create gemfile
+
+gem "colorize"
+#run bundle install to install the gems in the gemfile
+#will creat gemfile.lock that shows the bundle installed
--- main.rb
+++ main.rb
@@ -1,28 +1,31 @@
+require "colorize"
+
 def puts_git(cmd)
   puts `git #{cmd} -h`
 end
 
 def menu
+  puts "**Main Menu**".colorize(:color => :white, :background => :green)
   puts "Please select from the options below:"
-  puts "1: Enter git command"
+  puts "1: Enter git command".colorize(:green)
   puts "2: Exit"
   choice = gets.to_i
-  # print `clear`
-  # #backticks to call the command rather than print the string
-  sleep(3)
+  sleep(1)
   #sleep shows for designated amount of time (in seconds), and then clears
-  
+  print `clear`
+  # #backticks to call the command rather than print the string
+
   case choice
     when 1
-      print "Enter git command: "
+      puts "Enter git command: ".colorize(:green)
       puts_git(gets.strip)
       menu
     when 2
-      puts "Goodbye."
+      puts "Goodbye.".colorize(:blue)
       exit
     else
-      puts "Sorry, invalid choice."
-      puts "Please enter 1 or 2."
+      puts "Sorry, invalid choice.".colorize(:red)
+      puts "Please enter 1 or 2.".colorize(:green)
       menu
   end
 end
